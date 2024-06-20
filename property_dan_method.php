c779f9ce3aacaf221c4c2d188b471ae869dc8679
ChandraSiloto <Chandratiestto@gmail.com>
2024-06-20 17:12:53 +0700

    Property dan Method PHP

---
+++ property_dan_method.php
@@ -0,0 +1,47 @@
+<?php
+    
+    // class Produk { 
+
+    //     public $judul = 'Naruto' .  '<br>';
+    //     public $penulis = 'Masashi Kishimoto' . '<br>';
+    //     public $penerbit = 'Shueisha' . '<br>';  
+    //     public $harga = 'Rp '. 30000 ;
+        
+
+    //     public function getHello() {
+    //         // $Katakan_Halo = 'HelloWorld';
+    //         // return $Katakan_Halo;
+            
+    //         return '<br>'. 
+    //                 $this->judul . 
+    //                 $this->penulis . 
+    //                 $this->penerbit .
+    //                 $this->harga;
+           
+    //     }
+    // }
+
+    // $produk = new Produk();
+    // // $produk->judul = 'Shipuden';  // Menimpa value dari property judul
+    // var_dump($produk);
+    // // echo $produk->judul; // Naruto
+    // // // var_dump($produk->getHello());
+    // echo $produk->getHello();
+
+    
+    class Produk {
+        public $judul = 'Naruto';
+        public $penulis = 'Masashi Kishimoto';
+        public $penerbit = 'Shueisha';
+        public $harga = 30000 ;
+    }
+    
+
+    $produk = new Produk();
+    var_dump($produk);
+    $produk->judul = 'Shipuden';  // Menimpa value dari property judul
+    $produk->penulis = 'Masashi Kishimoto';
+    $produk->penerbit = 'Shueisha';
+    $produk->harga = 30000 ;
+    
+?>
